8978af8d7a0d9443f0ec160cc8353cbe36688719
FSeravat <felipecostatavares@gmail.com>
2022-12-23 15:51:30 -0300

    Downloader Done

---
+++ attPlugin.bat
@@ -0,0 +1,12 @@
+timeout 5
+
+setlocal
+
+chcp 1252
+G:
+cd G:\Drives compartilhados\Projetos_GASLUB_Operação_PTB17\03_TECNOLOGIA\download_for_drive
+start main.exe
+
+@pause
+
+endlocal
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from modules import Drive
-from modules import EmailSender
+import zipfile
 import json
 import os
 import logging
@@ -25,13 +25,25 @@ except FileNotFoundError:
 
 foldersName = data["files_name"]
 drivers = data["drivers"]
+folders = data["folders"]
 
 for d in drivers:
     drive = Drive.DriveService(d["id"])
     for file in foldersName:
-        fileList = drive.view_all_files(d["folderId"])
-        fileId = list(filter(lambda x:x["name"]==file+"_2020.zip", fileList))[0]
-        print(fileId["id"])
-        print(fileId["mimeType"])
-
-
+        for folder in folders:
+            if not os.path.exists(folder):
+                print(folder+" não existe no computador")
+                continue
+            version = folder.split("\\")
+            version = list(filter(lambda x:x.__contains__("Navisworks Manage"),version))[0][-4:]
+            fileName = file+"_"+version+".zip"
+            fileList = drive.view_all_files(d["folderId"])
+            fileId = list(filter(lambda x:x["name"]==fileName, fileList))[0]
+            zFile = drive.download_file(fileId["id"])
+            with open(os.path.join(folder, fileName),"wb") as f:
+                f.write(zFile)
+                f.close()
+            with zipfile.ZipFile(os.path.join(folder, fileName), mode="r") as archive:
+                archive.extractall(folder)
+                archive.close()
+                folderLocal = os.remove(os.path.join(folder, fileName))
